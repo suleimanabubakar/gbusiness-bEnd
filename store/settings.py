10edadc839d8fbde3baa7c12d8adb1c69eaefd56
suleimanabubakar <suleiman.u.abubakar@gmail.com>
2023-03-09 10:30:18 +0300

    token auth

--- store/settings.py
+++ store/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework_simplejwt',
+    # 'rest_framework_simplejwt',
+    'rest_framework.authtoken',
     'rest_framework',
     'debug_toolbar',
     'corsheaders',
@@ -176,9 +177,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+      'DEFAULT_AUTHENTICATION_CLASSES': (
+               'rest_framework.authentication.TokenAuthentication',
+    ),
 }
 
 
